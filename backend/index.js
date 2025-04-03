448d8afd13cad65125dc543db74faa48e9000a28
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2025-04-03 18:49:00 +0300

    Added some basic backend stuff

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,77 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const postgres = require('postgres')
 const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser')
+
+console.log(process.env.DB_PASS)
+
+const psql = postgres({
+  host: process.env.DB_HOST ?? "localhost",
+  port: process.env.DB_PORT ?? 5432,
+  database: process.env.DB_NAME ?? "woodcalculator",
+  username: process.env.DB_USER ?? "postgres",
+  password: process.env.DB_PASS,
+})
+
+async function initdb() {
+  await psql`
+    CREATE TABLE IF NOT EXISTS users (
+      id serial,
+      name varchar(30),
+      password char(60),
+      PRIMARY KEY(id)
+    );
+  `
+  await psql`
+    CREATE TABLE IF NOT EXISTS calculations (
+      id serial,
+      length double precision,
+      width double precision,
+      height double precision,
+      result double precision,
+      userid integer,
+      PRIMARY KEY(id),
+      CONSTRAINT fk_user FOREIGN KEY(userid)
+        REFERENCES users(id)
+    );
+  `
+}
+initdb()
+
 
 const app = express()
 app.use(bodyParser.urlencoded())
+app.use(cookieParser())
 
 app.post('/calculate', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
+  const passwordHash = await psql`
+    SELECT password FROM users WHERE name = ${req.body.name};
+  `
+  if (passwordHash.length === 0) {
+    res.status(400)
+    res.send()
+    return
+  }
+  const passwordMatch = await bcrypt.compare(req.body.pass, passwordHash[0].password)
+  if (passwordMatch) {
+    res.cookie("auth", "hello", {
+      maxAge: 1000*60*60,
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict"
+    })
+    res.send('Aye!')
+  } else {
+    res.status(400)
+    res.send('Nay.')
+  }
+})
+
+app.post('/checkauth', (req, res) => {
 
 })
 
