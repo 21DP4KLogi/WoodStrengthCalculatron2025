a2f982861439a74c7cf005c8a93286269147ecce
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2025-04-03 20:08:17 +0300

    Added authtokens and partially completed the calcution endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser')
 const postgres = require('postgres')
 const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
-
-console.log(process.env.DB_PASS)
+const crypto = require('node:crypto')
 
 const psql = postgres({
   host: process.env.DB_HOST ?? "localhost",
@@ -20,6 +19,7 @@ async function initdb() {
       id serial,
       name varchar(30),
       password char(60),
+      token char(16),
       PRIMARY KEY(id)
     );
   `
@@ -44,8 +44,33 @@ const app = express()
 app.use(bodyParser.urlencoded())
 app.use(cookieParser())
 
-app.post('/calculate', (req, res) => {
+async function authCookieValid(cookie) {
+  if (cookie === undefined) {
+    return false
+  }
+  const tokenBearer = await psql`
+    SELECT * FROM users WHERE token = ${cookie};
+  `
+  if (tokenBearer.length === 1) {
+    return true
+  } else {
+    return false
+  }
+}
 
+app.post('/calculate', async (req, res) => {
+  if (!await authCookieValid(req.cookies.auth)) {
+    res.status(401)
+    res.send()
+    return
+  }
+  const sentLength = req.body.length
+  const sentWidth = req.body.width
+  const sentHeight = req.body.height
+  const sentStrength = req.body.strength
+  const pretestibasMoments = (sentWidth * sentHeight ** 2) / 6
+  const pielaujamaSlodze = 8 * pretestibasMoments * (sentStrength / (sentLength ** 2))
+  res.send(pielaujamaSlodze)
 })
 
 app.post('/login', async (req, res) => {
@@ -59,12 +84,16 @@ app.post('/login', async (req, res) => {
   }
   const passwordMatch = await bcrypt.compare(req.body.pass, passwordHash[0].password)
   if (passwordMatch) {
-    res.cookie("auth", "hello", {
+    let cookieData = crypto.randomBytes(12).toString('base64')
+    res.cookie("auth", cookieData, {
       maxAge: 1000*60*60,
       httpOnly: true,
       secure: true,
       sameSite: "strict"
     })
+    await psql`
+      UPDATE users SET token = ${cookieData} WHERE name = ${req.body.name};
+    `
     res.send('Aye!')
   } else {
     res.status(400)
@@ -72,8 +101,13 @@ app.post('/login', async (req, res) => {
   }
 })
 
-app.post('/checkauth', (req, res) => {
-
+app.post('/checkauth', async (req, res) => {
+  if (await authCookieValid(req.cookies.auth)) {
+    res.send()
+  } else {
+    res.status(401)
+    res.send()
+  }
 })
 
 app.listen(3000)
